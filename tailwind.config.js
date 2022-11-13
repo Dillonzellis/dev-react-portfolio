a4e9c2d4c1b87de3fcec8d50c6b7c3f767f44f49
Dillon Ellis <dillonzellis@gmail.com>
2022-11-13 14:41:34 -0500

    nav added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        burtons: "burtons",
+      },
+    },
   },
   plugins: [],
-}
+};
